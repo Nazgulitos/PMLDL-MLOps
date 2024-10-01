5cd4b44d7606a2cb7fd5c33b001c86d5d102d5c8
Salikhova Nazgul <nazgulka2005@gmail.com>
2024-10-01 23:55:49 +0300

    Add py script for web ap using streamlit

---
+++ code/deployment/app/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import requests
+from PIL import Image
+
+st.title("Blond or Brunnete Hair Classifier")
+
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    # Send the image to the FastAPI model for prediction
+    if st.button('Predict'):
+        files = {"file": uploaded_file.getvalue()}
+        response = requests.post("http://api:8000/predict/", files=files)
+        if response.status_code == 200:
+            prediction = response.json()['prediction']
+            st.success(f"Prediction: {prediction}")
+        else:
+            st.error("Error in prediction")
+            
